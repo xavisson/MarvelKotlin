6b98bd0dc5fd1bbebd354c3e72c231552fb8c42e
Javier del Palacio <javipalacio@gmail.com>
2018-10-19 12:12:00 +0200

    [logger] Logger wont share package with activities

--- app/src/main/java/com/xavisson/marvel/presentation/MainActivity.kt
+++ app/src/main/java/com/xavisson/marvel/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.xavisson.marvel
+package com.xavisson.marvel.presentation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.xavisson.marvel.R
 
 class MainActivity : AppCompatActivity() {
 
