3821219b99e460ffb9a82f519eeb1da2c9b28a8c
Javier del Palacio <javipalacio@gmail.com>
2018-10-24 15:35:07 +0200

    [characterdetail] Send characterId to detailview

--- app/src/main/java/com/xavisson/marvel/presentation/navigator/ActivityNavigator.kt
+++ app/src/main/java/com/xavisson/marvel/presentation/navigator/ActivityNavigator.kt
@@ -3,10 +3,11 @@ package com.xavisson.marvel.presentation.navigator
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import com.xavisson.marvel.presentation.characterdetail.CharacterDetailActivity
+import com.xavisson.marvel.presentation.navigator.IntentExtras.CHARACTER_ID
 import java.lang.ref.WeakReference
 
 interface ActivityNavigator {
-    fun goToCharacterDetail()
+    fun goToCharacterDetail(characterId: Int)
 }
 
 class ApplicationActivityNavigator(
@@ -17,8 +18,13 @@ class ApplicationActivityNavigator(
     private val activity: AppCompatActivity?
         get() = activityRef.get()
 
-    override fun goToCharacterDetail() {
+    override fun goToCharacterDetail(characterId: Int) {
         val intent = Intent(activity, CharacterDetailActivity::class.java)
+        intent.putExtra(CHARACTER_ID, characterId)
         activity?.startActivity(intent)
     }
+}
+
+object IntentExtras {
+    const val CHARACTER_ID = "CHARACTER_ID"
 }
